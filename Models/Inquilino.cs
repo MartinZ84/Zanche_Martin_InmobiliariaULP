b0816c6e8ab1805bcc88a6df0d804100c9372a29
Martin Zanche <martinzanche@gmail.com>
2022-05-29 16:56:54 -0300

    api moificacion de controlasdor inquilino

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -23,14 +23,7 @@ public class Inquilino
  [Display(Name= "Lugar de trabajo")]
  [Required]
   public string ? Lugar_Trabajo { get; set; }
-//  [Display(Name= "DNI Garante")]
-// public string ? Dni_Garante { get; set; }
-// [Display(Name= "Nombre Garante")]
-// public string ? Nombre_Garante { get; set; } 
-// [Display(Name= "Apellido Garante")]
-// public string ? Apellido_Garante { get; set; }
-// [Display(Name= "Teléfono Garante")]
-// public string ? Telefono_Garante { get; set; }
+
 
 
 
